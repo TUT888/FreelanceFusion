48bfbd3206ce139d6f0ed0a60cd297d865473117
nguyenhuong <iwsj4redvelvet@gmail.com>
2024-09-16 14:59:44 +1000

    update navbar

--- routers/homeRouter.js
+++ routers/homeRouter.js
@@ -6,4 +6,14 @@ router.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
+// Serve the sign in page
+router.get('/sign-in', (req, res) => {
+    res.render('sign-in.ejs');
+});
+
+// Serve the register page
+router.get('/register', (req, res) => {
+    res.render('register.ejs');
+});
+
 module.exports = router;
